2a848cc2ae1277af2c6fa5f0a474fdc62da0c1e4
Kuldeep Meena <93571795+kuldeepdelhi@users.noreply.github.com>
2024-05-23 21:33:31 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,10 +8,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors({
-    origin: 'http://manpower.bigjack.fun',
-    methods: 'POST' // Only allow POST requests from the specified origin
-})); // Enable all CORS requests
+app.use(cors()); // Enable all CORS requests from any origin
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
